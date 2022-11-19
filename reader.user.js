be1f63888f66c411b65e7d1014878ea1b7180f02
atan <tanshiqi@gmail.com>
2022-11-19 16:16:36 +0800

    add author

--- reader.user.js
+++ reader.user.js
@@ -3,56 +3,71 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Just a Reader for TJAD via Chrome
-// @author       You
+// @author       atan
 // @match        http://*.tjad.cn/*
 // @grant        none
 // @require      https://cdn.jsdelivr.net/npm/php-unserialize@0.0.1/php-unserialize.min.js
 // @require      https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/crypto-js.min.js
 // ==/UserScript==
 
+(function () {
+    "use strict";
 
-(function() {
-    'use strict';
-
-    function safeUrl(str){
-        return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+    function safeUrl(str) {
+        return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/, "");
     }
 
-    function genNewUrl(oldUrl){
-        let key = CryptoJS.enc.Utf8.parse('itMkwgYdmMU3afKU');
-        return 'http://pdf.pinweb.io/n/'+safeUrl(CryptoJS.AES.encrypt(JSON.stringify(oldUrl), key, {mode: CryptoJS.mode.ECB}).toString());
+    function genNewUrl(oldUrl) {
+        let key = CryptoJS.enc.Utf8.parse("itMkwgYdmMU3afKU");
+        return (
+            "http://pdf.pinweb.io/n/" +
+            safeUrl(
+                CryptoJS.AES.encrypt(JSON.stringify(oldUrl), key, {
+                    mode: CryptoJS.mode.ECB,
+                }).toString()
+            )
+        );
     }
 
     // Your code here...
     function addReaderLink() {
-        var as = document.querySelectorAll('a[data-size]')
-        for(var i=0;i<as.length;i++){
+        var as = document.querySelectorAll("a[data-size]");
+        for (var i = 0; i < as.length; i++) {
+            let newUrl = "#";
 
-            let newUrl="#";
-
-            if(as[i].href.startsWith('http://read.tjad.cn/')){
+            if (as[i].href.startsWith("http://read.tjad.cn/")) {
                 newUrl = genNewUrl(parseLink(as[i].href));
-                as[i].parentElement.insertAdjacentHTML('beforeend','<a href="'+newUrl+'" target="_blank" class="ml-4 text-red-600">测试新版阅读</a>')
-            }else{
+                as[i].parentElement.insertAdjacentHTML(
+                    "beforeend",
+                    '<a href="' +
+                        newUrl +
+                        '" target="_blank" class="ml-4 text-red-600">测试新版阅读</a>'
+                );
+            } else {
                 let size = as[i].dataset.size;
-                if (size<20000000) {
+                if (size < 20000000) {
                     let payload = {
-                        doc:as[i].href.split("?")[0]+'?doc-convert/preview',
-                        u:'vadmin'
-                    }
+                        doc: as[i].href.split("?")[0] + "?doc-convert/preview",
+                        u: "vadmin",
+                    };
                     newUrl = genNewUrl(payload);
-                    as[i].parentElement.insertAdjacentHTML('beforeend','<a href="'+newUrl+'" target="_blank" class="ml-4 text-red-600">测试新版阅读</a>')
+                    as[i].parentElement.insertAdjacentHTML(
+                        "beforeend",
+                        '<a href="' +
+                            newUrl +
+                            '" target="_blank" class="ml-4 text-red-600">测试新版阅读</a>'
+                    );
                 }
                 //
             }
-
         }
     }
 
-    function parseLink(link){
-        return PHPUnserialize.unserialize(atob(link.replace('http://read.tjad.cn/t/','')));
+    function parseLink(link) {
+        return PHPUnserialize.unserialize(
+            atob(link.replace("http://read.tjad.cn/t/", ""))
+        );
     }
-
 
     document.addEventListener("inertia:success", (event) => {
         addReaderLink();
